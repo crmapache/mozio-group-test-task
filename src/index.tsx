5c9c9d90ce63618bf3333f64881e11645d4066b3
Maksim Zolotoi <crmapache@icloud.com>
2022-11-07 20:37:27 -0600

    Fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers'
@@ -10,10 +10,10 @@ import { GlobalStyle } from './styles/globalStyles'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <GlobalStyle />
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <App />
     </LocalizationProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
